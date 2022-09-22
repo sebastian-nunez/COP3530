96f1e594cb99f5e20425ec44c3d80e349811697a
Sebastian Nunez <02snunez@gmail.com>
2022-09-22 13:55:59 -0400

    09.22.22 - more queues

--- Programs/Prog16_01/Prog16_01.java
+++ Programs/Prog16_01/Prog16_01.java
@@ -2,17 +2,24 @@
 public class Prog16_01 {
 
     public static void main(String[] args) {
-        Queue q = new Queue();
+        Queue q = new Queue(3);
 
         //Insertion
-        for (int i = 0; i < 10; i++) {
-            int x = 32 + (int) (Math.random() * 95);
-            System.out.println(x + " --> " + (char) x);
-            q.enqueue((char) x);
-        }
+        // for (int i = 0; i < 10; i++) {
+        //     int x = 32 + (int) (Math.random() * 95);
+        //     System.out.println(x + " --> " + (char) x);
+        //     q.enqueue((char) x);
+        // }
+        q.enqueue('a');
+        q.enqueue('h');
+        q.enqueue('z');
+        q.print();
+        q.enqueue('r');
+        q.clear();
 
         //Display and removal
-        for (int i = 0; i < 10; i++) {
+        System.out.println(q.getSize());
+        for (int i = 0; i <= q.getSize(); i++) {
             System.out.println("Item at the front: " + q.getFront());
 
             q.dequeue();
--- Programs/Prog16_01/Queue.java
+++ Programs/Prog16_01/Queue.java
@@ -1,4 +1,8 @@
 public class Queue {
+    private char[] list;
+    private int capacity;
+    private int size;
+    private int front, back;
 
     public Queue() {
         capacity = 100;
@@ -20,17 +24,42 @@ public class Queue {
     }
 
     public void enqueue(char c) {
+        if (size == capacity) {
+            System.out.println("Queue is Full! Unable to add '" + c + "'");
+            return;
+        }
+
         back = (back + 1) % capacity;
         list[back] = c;
         size++;
     }
 
     public void dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            return;
+        }
+
         front = (front + 1) % capacity;
         size--;
     }
 
+    public void clear() {
+        front = 0;
+        back = size - 1;
+        size = 0;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++)
+            System.out.println(list[(front + i) % capacity]);
+    }
+
     public char getFront() {
+        if (isEmpty()) {
+
+        }
+
         return list[front];
     }
 
@@ -38,8 +67,8 @@ public class Queue {
         return size == 0;
     }
 
-    private char[] list;
-    private int capacity;
-    private int size;
-    private int front, back;
+    public int getSize() {
+        return this.size;
+    }
+
 }
