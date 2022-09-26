aabe60c59263aab4fa2bfa8cca11c71ed8b4e0c5
Sebastian Nunez <02snunez@gmail.com>
2022-09-26 00:22:09 -0400

    trie

--- Python/Trie Test/Trie.py
+++ Python/Trie Test/Trie.py
@@ -1,25 +1,49 @@
+optimized_count = 0
+naive_count = 0
+
+
 def main():
+    global naive_count
+    global optimized_count
+
     trie = Trie()
-    trie.insert('hello')
-    trie.insert('hi')
-    trie.insert('low')
-    trie.insert('loco')
-    trie.insert('')
+    file = open("./words.txt", "r")
 
-    trie.print_preorder(trie.root)
+    words_list = []
+    for word in file:
+        word = word.strip().lower()
 
-    print(f'"hello": {trie.search("hello")}')
-    print(f'"low": {trie.search("low")}')
-    print(f'"loco": {trie.search("loco")}')
-    print(f'"loco!": {trie.search("loco!")}')
-    print(f'"apple": {trie.search("apple")}')
-    print(f'"": {trie.search("")}')
+        trie.insert(word)
+        words_list.append(word)
 
-    print(f'"locomotive": {trie.search("locomotive")}')
-    print(f'"hiya": {trie.search("hiya")}')
-    print(f'"hi": {trie.search("hi")}')
-    print(f'prefix "hell": {trie.prefix("hell")}')
-    print(f'prefix "helo": {trie.prefix("helo")}')
+    while True:
+        naive_count = 0
+        optimized_count = 0
+
+        word = input('Enter a word (type "!q" to quit):: ').strip().lower()
+
+        if word == '!q':
+            file.close()
+            exit()
+
+        if not word.isalpha():
+            print('Only letters [a-z] are allowed! Try again...')
+            continue
+
+        is_valid_word = trie.search(word)
+        for w in words_list:
+            naive_count += 1
+            if w == word:
+                break
+
+        if is_valid_word:
+            print(f'{word} is a valid word!')
+        else:
+            print(f'{word } is NOT a valid word!')
+
+        print(f'Number of comparisons for "naive" solution: {naive_count}')
+        print(
+            f'Number of comparisons for "optimized" solution: {optimized_count}')
 
 
 class Node:
@@ -48,11 +72,15 @@ class Trie:
         return True
 
     def search(self, word: str) -> bool:
+        global optimized_count
+
         if word == '':
             return False
 
         current = self.root
         for char in word:
+            optimized_count += 1
+
             if char not in current.children:
                 return False
 
