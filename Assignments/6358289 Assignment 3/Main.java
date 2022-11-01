c142becf5cc11313a96119f9d54a0165cfbfbf67
Sebastian Nunez <02snunez@gmail.com>
2022-11-01 10:01:45 -0400

    started assignment 3

---
+++ Assignments/6358289 Assignment 3/Main.java
@@ -0,0 +1,77 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        // final int DATASET_SIZE = 10000000; // actual (int) 2e7
+        final int DATASET_SIZE = (int) 2e7;
+        final int SAMPLE_SIZE = 2500;
+
+        String outputFilename = "output.csv";
+        PrintWriter output = null;
+
+        //open output stream
+        try {
+            output = new PrintWriter(new FileWriter(outputFilename));
+        } catch (IOException ex) {
+            System.exit(1);
+        }
+
+        // dataset
+        int[] dataSet = new int[DATASET_SIZE];
+        SearchingAlgorithms.fillArray(dataSet, dataSet.length);
+
+        for (int k = 1; k < SAMPLE_SIZE; k++) {
+            // variables needed to time the program
+            Date date;
+            long startTime, endTime, elapsedTime;
+
+            // fill the arrays
+            int[] dataSource = new int[k];
+            SearchingAlgorithms.fillArray(dataSource, 2 * dataSet.length);
+
+            // linear search performace
+            date = new Date();
+            startTime = date.getTime();
+
+            for (int i = 0; i < k; i++) {
+                SearchingAlgorithms.sequentialSearch(dataSet, dataSource[i]);
+            }
+
+            date = new Date();
+            endTime = date.getTime();
+
+            elapsedTime = endTime - startTime;
+            output.print(k + ", " + (elapsedTime / 1000.0) + ", ");
+
+            // binary search performance
+            int[] sortedDataSet = Arrays.copyOf(dataSet, dataSet.length);
+
+            date = new Date();
+            startTime = date.getTime();
+
+            SearchingAlgorithms.quickSort(sortedDataSet); // sort the data once
+            for (int i = 0; i < k; i++) {
+                SearchingAlgorithms.binarySearch(sortedDataSet, dataSource[i]);
+            }
+
+            date = new Date();
+            endTime = date.getTime();
+
+            elapsedTime = endTime - startTime;
+            output.println(elapsedTime / 1000.0);
+
+            output.flush();
+        }
+
+        //close output stream
+        output.close();
+    }
+}
---
+++ Assignments/6358289 Assignment 3/SearchingAlgorithms.java
@@ -0,0 +1,124 @@
+import java.util.Random;
+
+/**
+ * Sebastian Nunez (6358289)
+ * COP3530
+ * Assignment 3
+ */
+public class SearchingAlgorithms {
+    /**
+     * Runs a iterative binary search operation for a specified value in the search space.
+     * Note: search space must be sorted in asending order!
+     * @param list search space
+     * @param x target element
+     * @return target was found in the search space
+     * @author Sebastian Nunez
+     */
+    public static boolean binarySearch(int[] list, int x) {
+        int lower = 0;
+        int upper = list.length - 1;
+
+        while (lower <= upper) {
+            int pivot = findPivotLocation(lower, upper);
+
+            if (list[pivot] == x) {
+                return true;
+            } else if (x < list[pivot]) {
+                upper = pivot - 1;
+            } else {
+                lower = pivot + 1;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Runs a ilinear search operation for a specified value in the search space.
+     * @param list search space
+     * @param x target element
+     * @return target was found in the search space
+     * @author Sebastian Nunez
+     */
+    public static boolean sequentialSearch(int[] list, int x) {
+        for (int i = 0; i < list.length; i++)
+            if (list[i] == x)
+                return true;
+
+        return false;
+    }
+
+    /**
+     *
+     * @param list reference to the list to be filled.
+     * @param range specifies the allowed range of values (inclusive)
+     * @author Sebastian Nunez
+     */
+    public static void fillArray(int[] list, int range) {
+        Random rand = new Random();
+        for (int i = 0; i < list.length; i++)
+            list[i] = rand.nextInt(range + 1); // add 1 to make it inclusive of the upper bound
+    }
+
+    /**
+     * Prints a visual representation of a given lists to the console.
+     * @param list reference to the list to be printed
+     * @author Sebastian Nunez
+     */
+    public static void printArray(int[] list) {
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+        System.out.println();
+    }
+
+    /**
+    * Recursive quickSort algorithm
+    * @author Prof. A. Hernandez
+    */
+    public static void quickSort(int[] list) {
+        quicksort(list, 0, list.length - 1);
+    }
+
+    private static void quicksort(int[] list, int begin, int end) {
+        int temp;
+        int pivot = findPivotLocation(begin, end);
+        // swap list[pivot] and list[end]
+        temp = list[pivot];
+        list[pivot] = list[end];
+        list[end] = temp;
+        pivot = end;
+        int i = begin,
+                j = end - 1;
+        boolean iterationCompleted = false;
+        while (!iterationCompleted) {
+            while (list[i] < list[pivot])
+                i++;
+            while ((j >= 0) && (list[pivot] < list[j]))
+                j--;
+            if (i < j) {
+                //swap list[i] and list[j]
+                temp = list[i];
+                list[i] = list[j];
+                list[j] = temp;
+                i++;
+                j--;
+            } else
+                iterationCompleted = true;
+        }
+        //swap list[i] and list[pivot]
+        temp = list[i];
+        list[i] = list[pivot];
+        list[pivot] = temp;
+        if (begin < i - 1)
+            quicksort(list, begin, i - 1);
+        if (i + 1 < end)
+            quicksort(list, i + 1, end);
+    }
+
+    /*
+    * Computes the pivot
+    */
+    private static int findPivotLocation(int b, int e) {
+        return (b + e) / 2;
+    }
+}
